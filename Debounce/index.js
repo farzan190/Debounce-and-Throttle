5d8b8eb5c2a132431510aa12e8e8e029b5b69076
farzan190 <farzanxavier@gmail.com>
2023-11-15 15:20:23 +0530

    added throttle functionality

--- Debounce/index.js
+++ Debounce/index.js
@@ -3,7 +3,7 @@
 // show button pressed "x"times every time button is pressed 
 //increase "triggered "Y" times" count after every 800ms of debounce 
 // every time a button is pressed x count will increase 
-// and if we pause for more than 800ms then y count will increase 
+// and if we pause for more than 800ms then y count will increase by 1
 
 
 const btn=document.querySelector(".increment");
@@ -12,13 +12,31 @@ const count=document.querySelector(".triggeredpressed");
 
 var pressedCount=0;
 var triggeredCount=0;
-const debounceCount=_.debounce(()=>{
-    count.innerHTML=++triggeredCount;
-},800);
+/*const debounceCount=_.debounce(()=>{
+    count.innerHTML=++triggeredCount;// we are increasing this after every pause for more than 800ms
+},800); 
 
 btn.addEventListener("click",()=>{
 
     btnpress.innerHTML=++pressedCount;
    debounceCount();
 
+});*/
+
+
+// create a button UI and add throttle as follows 
+// show button pressed "x"times every time button is pressed 
+//increase "triggered "Y" times" count after every 800ms of throttle
+// difference between both is that it doesnt wait for my pause it after some clicks
+  // suppose you are scrolling scrolling(calling for api for 800ms atleast then as soon as 800s hits it will call the api  thus we are showing it here as the count increase ) then after 800ms this will call the api 
+const throttleCount=_.throttle(()=>{
+    count.innerHTML=++triggeredCount;// we are increasing this after every pause for more than 800ms
+    
+},800);
+
+
+btn.addEventListener("click",()=>{
+
+    btnpress.innerHTML=++pressedCount;
+throttleCount();
 });
